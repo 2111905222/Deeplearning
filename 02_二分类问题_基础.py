7c80143d9ff4a23d34ab9dc24397e0315aeab506
cwx <gydxcwx@163.com>
2020-03-13 22:16:47 +0800

    添加Dropout看一下效果，不是特别理想

--- 02_二分类问题_基础.py
+++ 02_二分类问题_基础.py
@@ -21,9 +21,21 @@ from keras import models
 from keras import layers
 model = models.Sequential()
 model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
+model.add(layers.Dropout(0.5))
 model.add(layers.Dense(16, activation='relu'))
+model.add(layers.Dropout(0.5))
 model.add(layers.Dense(1, activation='sigmoid'))
 model.compile(optimizer='rmsprop',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+model_d = models.Sequential()
+model_d.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
+model_d.add(layers.Dropout(0.5))
+model_d.add(layers.Dense(16, activation='relu'))
+model_d.add(layers.Dropout(0.5))
+model_d.add(layers.Dense(1, activation='sigmoid'))
+model_d.compile(optimizer='rmsprop',
               loss='binary_crossentropy',
               metrics=['accuracy'])
 x_val = x_train[:10000]  # 将
@@ -33,18 +45,31 @@ y_val = y_train[:10000]  # 前10000的当做检验集
 partial_y_train = y_train[10000:]  # 10000-25000的留下当做训练集
 print(partial_x_train.shape,partial_y_train.shape)
 
-history = model.fit(partial_x_train, partial_y_train, epochs=3, batch_size=512,
+history = model.fit(partial_x_train, partial_y_train, epochs=10, batch_size=512,
                     validation_data=(x_val, y_val))
+history_d = model_d.fit(partial_x_train, partial_y_train, epochs=10, batch_size=512,
+                    validation_data=(x_val, y_val))
+
 import matplotlib.pyplot as plt
 history_dict = history.history
-print(history_dict.keys())
+history_d_dict = history_d.history
+loss_values_d = history_d_dict['loss']
+val_loss_values_d = history_d_dict['val_loss']
+
+
 loss_values = history_dict['loss']
 val_loss_values = history_dict['val_loss']
+
 acc = history_dict['accuracy']
 val_acc = history_dict['val_accuracy']
+acc_d = history_d_dict['accuracy']
+val_acc_d = history_d_dict['val_accuracy']
+
 epochs = range(1, len(loss_values) + 1)  # 设定1-20 ，x轴
 plt.plot(epochs, loss_values, 'bo', label='Training loss')  # x轴，y轴，bo为点
-plt.plot(epochs, val_loss_values, 'b', label='Validation loss')  # b为线
+plt.plot(epochs, val_loss_values, 'b-', label='Validation loss')  # b为线
+plt.plot(epochs, loss_values_d, 'ro', label='Training_d loss')  # x轴，y轴，bo为点
+plt.plot(epochs, val_loss_values_d, 'r-', label='Validation_d loss')  # b为线
 plt.xlabel('Epochs')
 plt.ylabel('Loss')
 plt.legend()
@@ -52,6 +77,8 @@ plt.show()
 plt.clf()
 plt.plot(epochs, acc, 'bo', label='Traning acc')
 plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.plot(epochs, acc_d, 'r+', label='Traning_d acc')
+plt.plot(epochs, val_acc_d, 'r-', label='Validation_d acc')
 plt.xlabel('Epochs')
 plt.ylabel('Accuracy')
 plt.legend()
