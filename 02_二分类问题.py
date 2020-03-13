58a579287f01a961ae300a206d56a4adc77cee39
cwx <gydxcwx@163.com>
2020-03-13 19:15:44 +0800

    (1)字典取值可以用.value()再用方法list()转化成list提取 (2)history.history得到的是字典，用key取值后得到的是列表 (3)分清楚()和[]，列表和元组都可以用[],但是字典不可以

--- 02_二分类问题.py
+++ 02_二分类问题.py
@@ -24,21 +24,10 @@ from keras import models
 from keras import layers
 
 
-def original_model():
+def create_model(layer_num):
     model = models.Sequential()
-    model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
-    model.add(layers.Dense(16, activation='relu'))
-    model.add(layers.Dense(1, activation='sigmoid'))
-    model.compile(optimizer='rmsprop',
-                  loss='binary_crossentropy',
-                  metrics=['accuracy'])
-    return model
-
-
-def Smaller_model():
-    model = models.Sequential()
-    model.add(layers.Dense(4, activation='relu', input_shape=(10000,)))
-    model.add(layers.Dense(4, activation='relu'))
+    model.add(layers.Dense(layer_num, activation='relu', input_shape=(10000,)))
+    model.add(layers.Dense(layer_num, activation='relu'))
     model.add(layers.Dense(1, activation='sigmoid'))
     model.compile(optimizer='rmsprop',
                   loss='binary_crossentropy',
@@ -52,37 +41,49 @@ partial_x_train = x_train[10000:]
 y_val = y_train[:10000]  # 前10000的当做检验集
 partial_y_train = y_train[10000:]  # 10000-25000的留下当做训练集
 print(partial_x_train.shape, partial_y_train.shape)
-model = original_model()
-model1 = Smaller_model()
-history = model.fit(partial_x_train, partial_y_train, epochs=20, batch_size=512,
-                    validation_data=(x_val, y_val))
-history1 = model1.fit(partial_x_train, partial_y_train, batch_size=512, epochs=20,
-                      validation_data=(x_val, y_val))
-
+original_model = create_model(16)
+Smaller_model = create_model(4)
+bigger_model = create_model(512)
+original_history = original_model.fit(partial_x_train, partial_y_train, epochs=20, batch_size=512,
+                                      validation_data=(x_val, y_val))
+smaller_history = Smaller_model.fit(partial_x_train, partial_y_train, batch_size=512, epochs=20,
+                                  validation_data=(x_val, y_val))
+bigger_history = bigger_model.fit(partial_x_train, partial_y_train, batch_size=512, epochs=20,
+                                  validation_data=(x_val, y_val))
 # 绘制图像
 import matplotlib.pyplot as plt
 
 
-def plot_data(history_dict, history_dict1, shape='b',
-              label_name='Validation loss', label_name1='Validation loss'):
-    if not(isinstance(shape, str)):
+def plot_data(history_dict,  shape, **label_name):
+    if not (isinstance(shape, str)):
         shape = 'b'
-    if not (isinstance(label_name, str)):
-        label_name = 'Validation loss'
-    loss_values = history_dict['loss']
-    val_loss_values = history_dict['val_loss']
-    loss_values1 = history_dict1['loss']
-    val_loss_values1 = history_dict1['val_loss']
-    acc = history_dict['accuracy']
-    val_acc = history_dict['val_accuracy']
-    epochs = range(1, len(loss_values) + 1)  # 设定1-20 ，x轴
-    plt.plot(epochs, val_loss_values, shape, label=label_name)  # x轴，y轴，bo为点
-    plt.plot(epochs, val_loss_values1, 'b', label=label_name1)  # b为线
+    loss_values = []
+    val_loss_values = []
+    acc = []
+    val_acc = []
+    for i in range(len(history_dict)):
+        history = history_dict[i]
+        print(type(history))
+        loss_values.append(history['loss'])
+        val_loss_values.append(history['val_loss'])
+        acc.append(history['accuracy'])
+        val_acc.append(history['val_accuracy'])
+    epochs = range(1, len(loss_values[0]) + 1)  # 设定1-20 ，x轴
+    color = ['bo-', 'ro-', 'yo-']
+    label_names = list(label_name.values())
+    for i in range(len(loss_values)):
+        plt.plot(epochs, loss_values[i], color[i], label=label_names[i])
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.show()
+    plt.clf()
+    for i in range(len(val_loss_values)):
+        plt.plot(epochs, val_loss_values[i], color[i], label=label_names[i])
     plt.xlabel('Epochs')
     plt.ylabel('Validation Loss')
     plt.legend()
     plt.show()
-    # plt.clf()
     # plt.plot(epochs, acc, 'bo', label='Traning acc')
     # plt.plot(epochs, val_acc, 'b', label='Validation acc')
     # plt.xlabel('Epochs')
@@ -91,13 +92,11 @@ def plot_data(history_dict, history_dict1, shape='b',
     # plt.show()
 
 
-history_dict1 = history1.history
-history_dict = history.history
-print(history_dict.keys())
-plot_data(history_dict, history_dict1, shape='bo', label_name='original', label_name1='smaller')
-prediction = model.predict(x_test)
-prediction1 = model1.predict(x_test)
-print('prediction[0]:', prediction[0], prediction[0].shape)
-print('prediction1[0]:', prediction1[0], prediction1[0].shape)
+history_dict = [original_history.history, smaller_history.history, bigger_history.history]
+plot_data(history_dict, shape='bo', label_name='original', label_name1='smaller', label_name2='bigger')
+# prediction = model.predict(x_test)
+# prediction1 = model1.predict(x_test)
+# print('prediction[0]:', prediction[0], prediction[0].shape)
+# print('prediction1[0]:', prediction1[0], prediction1[0].shape)
 # 输出一个概率，相当于二分类问题
 # 输出：prediction[0]: [0.24827671] (1,)，这就是一个好评与否的概率
