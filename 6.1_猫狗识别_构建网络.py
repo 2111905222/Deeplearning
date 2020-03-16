9de73caff645ac2d85ffc436ad01cd6c551a6a52
cwx <gydxcwx@163.com>
2020-03-16 11:01:12 +0800

    将基本操作包装成函数，更好滴调用

--- 6.1_猫狗识别_构建网络.py
+++ 6.1_猫狗识别_构建网络.py
@@ -14,7 +14,7 @@ test_dogs_dir = os.path.join(test_dir, 'dogs')
 from keras.preprocessing.image import ImageDataGenerator
 
 
-def data_preprocess():
+def data_preprocess():  # 从目标文件夹获取数据
     train_datagen = ImageDataGenerator(rescale=1. / 255)
     test_datagen = ImageDataGenerator(rescale=1. / 255)
     train_generator = train_datagen.flow_from_directory(
@@ -47,7 +47,7 @@ def plot_data(**data_monitor):
 
     i = 0
     for data in data_plot_value:
-        epochs = range(1, len(data)+1)  # 这里需要加1！！！！！！因为range不包括后一个数字本身
+        epochs = range(1, len(data) + 1)  # 这里需要加1！！！！！！因为range不包括后一个数字本身
         plt.plot(epochs, data, data_style[i], label=label_name[data_plot_key[i]])  # x轴和y轴一定要一样维度和大小！！！（epochs和data）
         i = i + 1
     plt.legend()
@@ -60,6 +60,45 @@ def plot_data(**data_monitor):
         i = i + 1
     plt.legend()
     plt.show()
+
+
+def data_preprocess_power():
+    train_datagen = ImageDataGenerator(  # 数据增强
+        rescale=1. / 255,
+        rotation_range=40,  # 角度值
+        width_shift_range=0.2,  # 图像水平和垂直方向上平移的范围（相对于总宽度和总高度的比例）
+        height_shift_range=0.2,
+        shear_range=0.2,  # 随机错切变换的角度
+        zoom_range=0.2,  # 图像随机缩放的范围
+        horizontal_flip=True,  # 随机将一半图像水平翻转，
+        fill_mode='nearest'  # 填充新创建像素的方法
+    )
+    test_datagen = ImageDataGenerator(rescale=1. / 255)  # 注意不能增强验证数据
+    train_generator = train_datagen.flow_from_directory(  # 以train_datagen模式从目标文件夹获取数据
+        train_dir,
+        target_size=(150, 150),
+        batch_size=32,
+        class_mode='binary'
+    )
+    validation_generator = test_datagen.flow_from_directory(  # 以validation_datagen模式从目标文件夹获取数据
+        validation_dir,
+        target_size=(150, 150),
+        batch_size=32,
+        class_mode='binary'
+    )
+    return train_generator, validation_generator
+
+
+def data_Statistics(history_datapower):
+    history_dict = history_datapower.history
+    loss_values = history_dict['loss']
+    val_loss_values = history_dict['val_loss']
+    acc = history_dict['accuracy']
+    val_acc = history_dict['val_accuracy']
+    label_name = {'acc': 'Traing acc', 'val_acc': 'Validation acc', 'loss_values': 'Traing loss',
+                  'val_loss_values': 'Validation loss'}
+    plot_data(acc=acc, val_acc=val_acc, loss_values=loss_values, val_loss_values=val_loss_values, label_name=label_name)
+
 
 from keras import layers
 from keras import models
@@ -74,6 +113,7 @@ model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(128, (3, 3), activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Flatten())
+model.add(layers.Dropout(0.5))
 model.add(layers.Dense(512, activation='relu'))  # 计算下来剩下28 * 28 所以需要512个神经元
 model.add(layers.Dense(1, activation='sigmoid'))
 print(model.summary())
@@ -84,19 +124,28 @@ model.compile(optimizer=optimizers.RMSprop(lr=1e-4),
               loss='binary_crossentropy',
               metrics=['accuracy'])
 
-train_data, validation_data = data_preprocess()
-history = model.fit_generator(
-    train_data,
+
+
+# train_data, validation_data = data_preprocess()
+# history = model.fit_generator(
+#     train_data,
+#     steps_per_epoch=100,
+#     epochs=20,
+#     validation_data=validation_data,
+#     validation_steps=50
+# )  # 100(steps_per_epch)*20=2000(总体的训练数据的大小)，及高速fit需要运行多少次梯度下降
+# model.save('cats_and_dogs_small_1.h5')
+
+train_generator, validation_generator = data_preprocess_power()  # 数据增强
+
+history_datapower = model.fit_generator(  # 使用数据增强来训练模型
+    train_generator,
     steps_per_epoch=100,
-    epochs=20,
-    validation_data=validation_data,
+    epochs=100,
+    validation_data=validation_generator,
     validation_steps=50
-)  # 100(steps_per_epch)*20=2000(总体的训练数据的大小)，及高速fit需要运行多少次梯度下降
-model.save('cats_and_dogs_small_1.h5')
-history_dict = history.history
-loss_values = history_dict['loss']
-val_loss_values = history_dict['val_loss']
-acc = history_dict['accuracy']
-val_acc = history_dict['val_accuracy']
-label_name = {'acc': 'Traing acc', 'val_acc': 'Validation acc', 'loss_values': 'Traing loss', 'val_loss_values': 'Validation loss'}
-plot_data(acc=acc, val_acc=val_acc, loss_values=loss_values, val_loss_values=val_loss_values, label_name=label_name)
+)
+
+model.save('cats_and_dogs_small_2.h5')  # 保存
+data_Statistics(history_datapower)
+
